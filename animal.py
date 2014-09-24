3b2ad3a8b507379aab87d4cb378f53b16d7a44e6
schiem <schiem01@gmail.com>
2014-09-24 18:09:21 +0800

    More progress in both the world and the animal.  I'm having difficulty keeping the interfaces separate, mostly because it's the easiest to just make the classes have full access to one another.  I know, it's a terrible practice, but I mostly want to get a working prototype whipped up for now. Python really lets you do a lot of things you really shouldn't, because you can (and people say C++ is bad when it comes that!).

--- animal.py
+++ animal.py
@@ -9,15 +9,21 @@
 import random
 
 class Animal:
-    def __init__(self, speed, size, death_age, ID, diet_type, cannibal, x, y):
-        self.speed = speed
-        self.size = size
-        self.death_age = death_age
+    def __init__(self, speed, size, sight, death_age, ID, diet_type, cannibal, x, y, world):
+        #things we can vary
+        self.genes = {"speed" : speed, \
+        "size" : size, \
+        "sight" : sight, \
+        "death_age" : death_age,\
+        "cannibal" : cannibal,\
+        "diet_type" : diet_type}
+        
         self.ID = ID
-        self.diet_type = diet_type
-        self.cannibal = cannibal
         self.x = x
         self.y = y
+        
+        #who relies on modularity anyway?
+        self.world = world
 
         #when energy reaches zero, you die
         self.energy = 100
@@ -29,6 +35,8 @@ class Animal:
         self.age = 0
         self.metabolism = (int) speed/size 
         self.gender = random.randint(0,1)
+        self.is_dead = False
+        self.child = []
 
     def act(self):
         #do action
@@ -45,24 +53,49 @@ class Animal:
 
     def breed(self, animal):
         #breed with an animal
+        #it takes two to tango, but both shouldn't produce offspring
+
 
     def search(self, obj):
         #find any object.  Could be food, could be an animal
+        
 
-    def move(self, location):
+    def move(self, x, y):
         #move to a location, if it is valid
+        if(validate_move(x, y)):
+            self.x = x
+            self.y = y
 
     def die(self):
         #kills the object
+        self.is_dead = True
 
     def can_breed(self, animal):
         #checks if a particular animal can breed with this one
-        return animal.ID == self.ID
+        return (type(animal) is Animal) and (animal.ID == self.ID) and (animal.gender != self.gender)
 
     def can_eat(self, obj):
         #checks if a particular object can be eaten by this animal
-    
+
+        #is it the same type as us?  If so, is it okay to eat it?
+        cannibalize = self.ID != obj.ID or self.cannibal == True
+
+        #Do we eat this type of object?
+        dietary_restriction = type(obj) in self.diet_type 
+        
+        #is it too big for us to eat?
+        size_restriction = self.size >= obj.size
+
+        #let's not eat our children or potential mates
+        is_child = obj in self.child
+        can_mate = can_breed(obj) 
+
+        return (cannibalize and dietary restriction and size_restriction and not is_child and not can_mate)
+
     def correct_energy(self):
         #change the current energy to max if we exceed the max energy
         if(self.energy > self.max_energy):
             self.energy = self.max_energy
+
+    def validate_move(self, x, y):
+        return (x <= world.width and x>=0) and (y <=world.height and y>=0)
--- world.py
+++ world.py
@@ -15,6 +15,7 @@ class World:
     def create_random_animal(self):
         speed = random.randint(1, 10)
         size = random.randint(1, 10)
+        sight = random.randint(1, 10)
         death_age = random.randint(1, 100)
         ID = generate_new_id(Animal)
         
@@ -29,13 +30,29 @@ class World:
 
         objects.append(Animal(speed, size, death_age, ID, diet_type, cannibal, x, y)
 
+    def get_id(self, obj):
+        for animal in objects:
+            if(type(animal) is Animal):
+                if(same_species(obj, animal)):
+                    return animal.ID
+        return None
+
+    def same_species(animal1, animal2):
+        is_same = True
+        for key in animal1.genes:
+            is_same = is_same and compare_stat(animal1.genes[key], animal2.genes[key]
+        return is_same
+
+    def compare_stat(stat1, stat2):
+        #is the difference more than 20%.  20% is too much
+        return abs((stat1-stat2)/((stat1 + stat2)/2)) > .2 
+    
     def generate_new_id(self, obj_type):
         #generate a new ID for a plant or animal
         #requires that the ID is not already in use
         ID = 1
         for obj in objects:
-            #I know, I know...old style classes
-            if isinstance(obj, obj_type):
+            if (type(obj) is obj_type):
                 if obj.ID == ID:
                     ID = ID + 1
         return ID
@@ -53,5 +70,7 @@ class World:
     def run_objects(self):
         for obj in objects:
             obj.act()
+            if(obj.is_dead):
+                objects.remove(obj)
 
 
